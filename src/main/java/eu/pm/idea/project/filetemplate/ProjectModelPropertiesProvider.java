a7bf8db5c38ac2c529e7c9440f5e4af75b4d5381
silviu <silviu>
2022-07-23 23:20:36 +0200

    find the version from the pom of the current (path) module

--- src/main/java/eu/pm/idea/project/filetemplate/ProjectModelPropertiesProvider.java
+++ src/main/java/eu/pm/idea/project/filetemplate/ProjectModelPropertiesProvider.java
@@ -4,6 +4,7 @@ import com.intellij.ide.fileTemplates.DefaultTemplatePropertiesProvider;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.FileIndexFacade;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiManager;
@@ -36,31 +37,48 @@ public class ProjectModelPropertiesProvider implements DefaultTemplateProperties
     @Override
     public void fillProperties(@NotNull PsiDirectory psiDirectory, @NotNull Properties properties) {
 
+
         defaultTemplateVariables = ProjectModelTemplateVariablesData.getInstance();//psiDirectory.getProject().getService(POMTemplateVariablesData.class);
 
+        //psiDirectory.getParentDirectory()
         final Project project = psiDirectory.getProject();
         DomManager domManager = DomManager.getDomManager(project);
 
-        setVersion(properties, project, domManager);
+        setVersion(psiDirectory, properties, project, domManager);
 
     }
 
-    private void setVersion(@NotNull Properties properties, Project project, DomManager domManager) {
+    /**
+     * todo :
+     * <p>
+     * is vFile in psiDirectory ?
+     * <p>
+     * PsiDirectory
+     */
+    private void setVersion(PsiDirectory psiDirectory, @NotNull Properties properties, Project project, DomManager domManager) {
         String candidateVersion = UNKNOWN_VALUE;
         ProjectScopeImpl scope = new ProjectScopeImpl(project, FileIndexFacade.getInstance(domManager.getProject()));
         Collection<VirtualFile> virtualFiles = FilenameIndex.getVirtualFilesByName("pom.xml", scope);
 
+
+        String currentFileRoot = ProjectFileIndex.SERVICE.getInstance(project).getContentRootForFile(psiDirectory.getVirtualFile()).getPath();
+
         for (VirtualFile vfile : virtualFiles) {
-            //OpenFileDescriptor ofd = new OpenFileDescriptor(project, vfile);
             try {
                 var psiFile = PsiManager.getInstance(project).findFile(vfile);
-                XmlFile asXMLFile = (XmlFile) psiFile;
+                String pomModuleRoot = ProjectFileIndex.SERVICE.getInstance(project).getContentRootForFile(vfile).getPath();
 
-                       DomFileElement domFileElement = domManager.getFileElement(asXMLFile, ProjectModelRoot.class);
+                if (currentFileRoot.equals(pomModuleRoot)) {
+
+                    XmlFile asXMLFile = (XmlFile) psiFile;
+                    DomFileElement domFileElement = domManager.getFileElement(asXMLFile, ProjectModelRoot.class);
 //                   DomFileElement domFileElement = domManager.getFileElement(asXMLFile);
 
-                ProjectModelRoot projectModelRoot = (ProjectModelRoot) domFileElement.getRootElement();
-                candidateVersion = projectModelRoot.getVersion() != null ? projectModelRoot.getVersion().getValue() : "unknown";
+                    if (domFileElement != null) {
+                        ProjectModelRoot projectModelRoot = (ProjectModelRoot) domFileElement.getRootElement();
+                        candidateVersion = projectModelRoot.getVersion() != null ? projectModelRoot.getVersion().getValue() : "unknown";
+                    }
+                }
             } catch (Exception e) {
                 logger.error("failed to get version with : " + e.getMessage(), e);
             }
