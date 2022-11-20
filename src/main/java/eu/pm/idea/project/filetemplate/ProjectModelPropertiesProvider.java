0837d72c2fcf695ed550e1fdacd79771a8f85785
silviu <silviu>
2022-11-20 20:10:13 +0100

    load gradle properties

--- src/main/java/eu/pm/idea/project/filetemplate/ProjectModelPropertiesProvider.java
+++ src/main/java/eu/pm/idea/project/filetemplate/ProjectModelPropertiesProvider.java
@@ -16,13 +16,16 @@ import com.intellij.util.xml.DomManager;
 import eu.pm.idea.project.configuration.ErrorNotifier;
 import eu.pm.idea.project.configuration.ProjectModelTemplateVariablesData;
 import eu.pm.idea.project.maven.ProjectModelRoot;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.FileReader;
 import java.util.Collection;
 import java.util.Properties;
 
 import static org.apache.commons.collections.CollectionUtils.isEmpty;
 import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
+import static org.apache.commons.lang3.StringUtils.defaultString;
 
 /**
  * property provider exposing project object model.
@@ -90,17 +93,17 @@ public class ProjectModelPropertiesProvider implements DefaultTemplateProperties
         String currentFileRoot = ProjectFileIndex.SERVICE.getInstance(project).getContentRootForFile(psiDirectory.getVirtualFile()).getPath();
 
         Collection<VirtualFile> pomVirtualFiles = FilenameIndex.getVirtualFilesByName("pom.xml", scope);
+        Collection<VirtualFile> gradlePropertiesVFs = FilenameIndex.getVirtualFilesByName("gradle.properties", scope);
         boolean errNotified = false;
 
 
         if (!isEmpty(pomVirtualFiles)) {
             errNotified = loadMavenVersion(project, domManager, currentFileRoot, pomVirtualFiles);
         } else {
-
-            Collection<VirtualFile> gradleVirtualFiles = FilenameIndex.getVirtualFilesByName("build.gradle", scope);
+            if (!isEmpty(gradlePropertiesVFs)) {
+                errNotified = loadGradle(project, domManager, currentFileRoot, gradlePropertiesVFs);
+            }
         }
-
-
 
         if (!errNotified && isNotEmpty(pomVirtualFiles) && simpleProjectDescriptor.getCandidateVersion().equals(UNKNOWN_VALUE)) {
             ErrorNotifier.notifyError(project, "version not identified.");
@@ -110,7 +113,41 @@ public class ProjectModelPropertiesProvider implements DefaultTemplateProperties
         properties.put(defaultTemplateVariables.getName(), simpleProjectDescriptor.getCandidateModuleName());
     }
 
+    private boolean loadGradle(Project project, DomManager domManager, String currentFileRoot, Collection<VirtualFile> gradlePropertiesVFs) {
+        boolean errNotified = false;
+
+        // check for properties and common naming
+        ProjectScopeImpl scope = new ProjectScopeImpl(project, FileIndexFacade.getInstance(domManager.getProject()));
+        for (VirtualFile vfile : gradlePropertiesVFs) {
+            try {
+                var psiFile = PsiManager.getInstance(project).findFile(vfile);
+                String pomModuleRoot = ProjectFileIndex.SERVICE.getInstance(project).getContentRootForFile(vfile).getPath();
+
+                if (currentFileRoot.equals(pomModuleRoot)) {
+                    Properties gradleProps = new Properties();
+                    gradleProps.load(new FileReader(vfile.getPath()));
+                    simpleProjectDescriptor.setCandidateVersion(defaultString(
+                            gradleProps.getProperty("snapshotVersion"),
+                            gradleProps.getProperty("version")
+                    ));
+                }
+            } catch (Throwable throwable) {
+                logger.error("failed to get version with : " + throwable.getMessage(), throwable);
+                ErrorNotifier.notifyError(project, throwable.getMessage());
+                errNotified = true;
+            }
+        }
+
+        // Load org.jetbrains.intellij.Version.tk from IntelliJPluginExtension
+        // see https://raw.githubusercontent.com/JetBrains/gradle-intellij-plugin/51ac90da2ede0c02bcadb42c03b156f61fdfe1b1/src/main/kotlin/org/jetbrains/intellij/IntelliJPlugin.kt
+
+
+
+        return errNotified;
+    }
+
     private boolean loadMavenVersion(Project project, DomManager domManager, String currentFileRoot, Collection<VirtualFile> pomVirtualFiles) {
+        ProjectScopeImpl scope = new ProjectScopeImpl(project, FileIndexFacade.getInstance(domManager.getProject()));
         boolean errNotified = false;
         for (VirtualFile vfile : pomVirtualFiles) {
             try {
